413e1ede096da4c1a86431ca488be1726a6c6d57
Zahir Junejo <zjunejo1993@gmail.com>
2017-01-07 02:25:46 +0500

    Enter Musician class

---
+++ www/js/Musician.js
@@ -0,0 +1,53 @@
+class Musician
+{
+    constructor(name, email, username, dob, password){
+    	this._name = name;
+    	this._email = email;
+    	this._username = username;
+    	this._dob = dob;
+    	this._password = password;
+    }
+
+
+    get name(){
+    	return this._name;
+    }
+
+    set name(name){
+    	return this._name = name;
+    }
+
+    get email(){
+    	return this._email;
+    }
+
+    set name(email){
+    	return this._email = email;
+    }
+
+    get username(){
+    	return this._username;
+    }
+
+    set name(username){
+    	return this._username = username;
+    }
+
+    get dob(){
+    	return this._dob;
+    }
+
+    set dob(dob){
+    	return this._dob = dob;
+    }
+
+    get password(){
+    	return this._password;
+    }
+
+    set password(password){
+    	return this._password = password;
+    }
+
+
+}
